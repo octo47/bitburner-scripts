e3ce32555f40f3b10a026a350f119e45d5e19890
Andrey Stepachev <octo47@gmail.com>
2022-11-03 23:34:28 +0000

    Improved analyse script

--- src/coordinate/analyse.ts
+++ src/coordinate/analyse.ts
@@ -1,48 +1,110 @@
-import { NS } from '@ns'
-import { HostList } from '../lib/hostdata'
+import { NS, Server } from '@ns'
+import { Scanner } from '/lib/scanner.js'
 
 export async function main(ns : NS) : Promise<void> {
-    const hostPaths = HostList.load(ns)
 
-    const hackedHosts = hostPaths.hosts.filter((el) => { 
-        return el.hacked && !el.owned
-    })
-    
-    const servers = hackedHosts.map((el) => { 
-            return ns.getServer(el.hostname)
-        }).filter((h) => h.moneyMax > 0)
-
-    const wa = ns.weakenAnalyze(1)
-    for (let i = 0; i < servers.length; i++) {
-        const server = servers[i]
-        const hostname = server.hostname
-        const money = server.moneyAvailable
-        const moneyMax = server.moneyMax
-
-        if (server.moneyAvailable / moneyMax < 0.5) {
-            const threadsToDouble = ns.growthAnalyze(hostname, 2)
-            const growTime = ns.getGrowTime(hostname) /1000/60
-            // we are going to grow by doubling, pure earnings is exactly the amount
-            const earningByThread = server.moneyAvailable / growTime
-    
-            console.log(hostname, {
-                threadsToDouble: threadsToDouble,
-                growTime: growTime,
-                earningByThread: earningByThread
-            })
-        } else {
-            const hackFraction = ns.hackAnalyze(hostname)
-            const hackAmount = hackFraction*money
-            const hackTime = ns.getHackTime(hostname)/1000/60
-            const hackChance = ns.hackAnalyzeChance(hostname)
-            const hackForSure = hackAmount * hackChance
-
-            console.log(hostname, { 
-                hackAmount: hackAmount,
-                hackTime: hackTime,
-                hackForSure: hackForSure
-            })
-
-        }
+    interface Capacity {
+        totalRam: number;
+        totalThreads: number;
+        scriptRam: number;
     }
+
+    interface Grow {
+        hostname: string;
+        growTimeWithCapacity: number;
+        moneyAvailable: number;
+        earnings: number;
+    }
+
+    interface Hack {
+        hostname: string;
+        hackAmount: number;
+        hackTime: number;
+        hackForSure: number;
+    }
+
+    type Action = Grow | Hack
+
+    function analyseCapacity(servers: Server[]): Capacity {
+        const workers = servers.filter((el) => { 
+            return el.hasAdminRights
+        })
+
+        const totalRam = workers.map((server) => server.maxRam).reduce((accum, val) => accum + val, 0)
+        const scriptRam = ns.getScriptRam("/worker/hack.js", "home")
+        const totalThreads = totalRam / scriptRam
+
+        const capacity = {
+            totalRam: totalRam,
+            totalThreads: totalThreads,
+            scriptRam: scriptRam
+        }
+
+        console.log(capacity)
+        return capacity
+    }
+
+    function analyseHacking(servers: Server[], capacity: Capacity): Action[] {
+        const hackable = servers.filter((el) => { 
+            return el.hasAdminRights && el.moneyMax > 0
+        })
+
+        const grows: Grow[] = []
+        const hacks: Hack[] = []
+        
+        const wa = ns.weakenAnalyze(1)
+        for (let i = 0; i < hackable.length; i++) {
+            const server = hackable[i]
+            const hostname = server.hostname
+            const money = server.moneyAvailable
+            const moneyMax = server.moneyMax
+    
+            if (server.moneyAvailable / moneyMax < 0.5) {
+                const threadsToDouble = ns.growthAnalyze(hostname, 2)
+                const growTime = ns.getGrowTime(hostname)
+                const growTimeWithCapacity = growTime * threadsToDouble / capacity.totalThreads / 1000 / 60
+                // we are going to grow by doubling, pure earnings is exactly the amount
+                const earnings = server.moneyAvailable / growTimeWithCapacity 
+        
+                const action: Grow = {
+                    hostname: server.hostname,
+                    growTimeWithCapacity: growTimeWithCapacity,
+                    moneyAvailable: server.moneyAvailable,
+                    earnings: earnings
+                }
+                grows.push(action)
+            } else {
+                const hackFraction = ns.hackAnalyze(hostname)
+                const hackAmount = hackFraction*money
+                const hackTime = ns.getHackTime(hostname)/1000/60
+                const hackChance = ns.hackAnalyzeChance(hostname)
+                const hackForSure = hackAmount * hackChance
+    
+                const action: Hack = { 
+                    hostname: server.hostname,
+                    hackAmount: hackAmount,
+                    hackTime: hackTime,
+                    hackForSure: hackForSure
+                }
+                hacks.push(action)
+    
+            }
+        }
+        grows.sort((a, b) => -(a.earnings - b.earnings))
+        grows.forEach((g) => console.log(g))
+        hacks.sort((a, b) => -(a.hackForSure - b.hackForSure))
+        hacks.forEach((g) => console.log(g))
+        return []
+    }
+
+    const servers = new Scanner()
+        .scan(ns)
+        .hosts
+        .map((item) => ns.getServer(item.hostname))
+
+
+    const capacity = analyseCapacity(servers)
+    analyseHacking(servers, capacity)
+
+
 }
