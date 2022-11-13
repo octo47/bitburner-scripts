e0ea5d6a28f9c44e0893a55b72c0a1a2af373680
Andrey Stepachev <octo47@gmail.com>
2022-11-13 10:55:33 +0000

    Sort by SOA rewards in infiltrations

--- src/formulas/infiltrations.ts
+++ src/formulas/infiltrations.ts
@@ -2,6 +2,6 @@ import { NS } from '@ns'
 
 export async function main(ns : NS) : Promise<void> {
     const infiltrations = ns.infiltration.getPossibleLocations().map((loc)=> ns.infiltration.getInfiltration(loc.name))
-    infiltrations.sort((a, b) => a.difficulty - b.difficulty)
+    infiltrations.sort((a, b) => a.reward.SoARep - b.reward.SoARep)
     console.log(infiltrations)
 }
